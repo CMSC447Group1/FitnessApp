69ac308070bf9c8e50293f7b7b6c04677b9c8549
gmfiga <98924344+gmartin3umbc@users.noreply.github.com>
2023-05-12 13:45:50 -0400

    using postgres

--- server/app/main.py
+++ server/app/main.py
@@ -3,9 +3,23 @@ import mysql.connector
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional, List
-
+import time
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+
+while True:
+    try:
+        conn = psycopg2.connect(host='dpg-chdueaak728nf3m74c10-a.oregon-postgres.render.com', database='workout_crud', user='root', password='4vCvzua06RTVJpCKbPnbq0c5bIG0Ahc2',
+                                cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error:", error)
+        time.sleep(60)
 
 
 class Workout(BaseModel):
@@ -30,20 +44,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-mydb = mysql.connector.connect(user='sql9617151', password='G9hf1EmwVx',
-                               host='sql9.freemysqlhosting.net', database='sql9617151')
+# mydb = mysql.connector.connect(user='sql9617151', password='G9hf1EmwVx',
+#                                host='sql9.freemysqlhosting.net', database='sql9617151')
 
-if mydb.is_connected():
-    print(mydb)
-    print('Connection Established')
+# if mydb.is_connected():
+#     print(mydb)
+#     print('Connection Established')
 
-cursor = mydb.cursor(dictionary=True)
+# cursor = mydb.cursor(dictionary=True)
 
 # cursor.execute('SET GLOBAL connect_timeout=288000000')
 # cursor.execute('SET GLOBAL interactive_timeout=28800000')
 # cursor.execute('SET GLOBAL wait_timeout=28800000')
 
-mydb.commit()
+# mydb.commit()
 
 
 @app.get('/')
@@ -53,15 +67,17 @@ def root():
 
 @app.get('/myworkouts')  # Get all workout sheets
 def get_all_workouts():
-    cursor.execute("SELECT * FROM workout_sheet")
+    conn.rollback()
+    cursor.execute("SELECT * FROM workout_crud.workout_sheet")
     workoutList = cursor.fetchall()
     return workoutList
 
 
 @app.get('/myworkouts/{id}')  # Get all workout sheets
 def get_workout(id: int):
+    conn.rollback()
     cursor.execute(
-        "SELECT exercise, reps, sets FROM workout_with_exercises WHERE id = %s", [str(id)])
+        "SELECT exercise, reps, sets FROM workout_crud.workout_with_exercises WHERE id = %s", [str(id)])
     workoutList = cursor.fetchall()
     if not workoutList:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -71,79 +87,83 @@ def get_workout(id: int):
 
 @app.delete("/myworkouts/{id}")  # Delete workout by id
 def delete_workout(id: int):
+    conn.rollback()
     cursor.execute(
-        "SELECT * from workout_sheet WHERE id = %s", [str(id)])
+        "SELECT * from workout_crud.workout_sheet WHERE id = %s", [str(id)])
     workoutList = cursor.fetchall()
     if not workoutList:
         raise HTTPException(status_code=404, detail="Workout not found")
     cursor.execute(
-        "DELETE FROM workout_exercise WHERE workout_id = %s", [str(id)])
+        "DELETE FROM workout_crud.workout_exercise WHERE workout_id = %s", [str(id)])
     cursor.execute(
-        "DELETE FROM workout_sheet WHERE id = %s", [str(id)])
-    mydb.commit()
+        "DELETE FROM workout_crud.workout_sheet WHERE id = %s", [str(id)])
+    conn.commit()
     return {"ok": True}
 
 
 @app.post("/myworkouts", status_code=status.HTTP_201_CREATED)
 def create_workout(workout: Workout):
-    cursor.execute("""INSERT INTO workout_sheet (name, description) VALUES (%s, %s) """,
+    conn.rollback()
+    cursor.execute("""INSERT INTO workout_crud.workout_sheet (name, description) VALUES (%s, %s) """,
                    (workout.name, workout.description))
     cursor.execute(
-        """ SELECT * FROM workout_sheet WHERE name = %s""", [workout.name])
+        """ SELECT * FROM workout_crud.workout_sheet WHERE name = %s""", [workout.name])
     workoutList = cursor.fetchone()
-    mydb.commit()
+    conn.commit()
     if workoutList:
         return workoutList
 
 
 @app.post("/myworkouts/exercises", status_code=status.HTTP_201_CREATED)
 def create_exercises(exercises: List[Exercise]):
-    cursor.execute("SELECT id FROM workout_sheet WHERE name = %s", [
+    conn.rollback()
+    cursor.execute("SELECT id FROM workout_crud.workout_sheet WHERE name = %s", [
                    exercises[0].workout_name])
     id = cursor.fetchone()['id']
 
     for exercise in exercises:
-        cursor.execute("""INSERT INTO workout_exercise (workout_id, exercise, reps, sets) VALUES (%s, %s, %s, %s) """,
+        cursor.execute("""INSERT INTO workout_crud.workout_exercise (workout_id, exercise, reps, sets) VALUES (%s, %s, %s, %s) """,
                        (id, exercise.exercise, exercise.reps, exercise.sets))
         cursor.execute(
-            """ SELECT exercise FROM workout_exercise WHERE exercise = %s""", [exercise.exercise])
+            """ SELECT exercise FROM workout_crud.workout_exercise WHERE exercise = %s""", [exercise.exercise])
         workoutList = cursor.fetchall()
-        mydb.commit()
+        conn.commit()
         if not workoutList:
             return {'message': 'exercise not created'}
     return {'message': 'all worked out'}
 
 
 @app.put("/myworkouts/{id}", status_code=status.HTTP_201_CREATED)
-def edit_workout(id: int, workout: Workout):
-
+def update_workout(id: int, workout: Workout):
+    conn.rollback()
     # check if workout exists
     cursor.execute(
-        "SELECT * from workout_sheet WHERE id = %s", [str(id)])
+        "SELECT * from workout_crud.workout_sheet WHERE id = %s", [str(id)])
     workoutList = cursor.fetchall()
     if not workoutList:
         raise HTTPException(status_code=404, detail="Workout not found")
 
     # update workout info
     cursor.execute(
-        "UPDATE workout_sheet SET `name` = %s, `description` = %s WHERE id = %s", [workout.name, workout.description, str(id)])
+        "UPDATE workout_crud.workout_sheet SET name = %s, description = %s WHERE id = %s", [workout.name, workout.description, str(id)])
 
-    mydb.commit()
+    conn.commit()
     return {"ok": True}
 
 
 @app.put("/myworkouts/exercises/{id}")
-def edit_exercises(id: int, exercises: List[Exercise]):
+def update_exercises(id: int, exercises: List[Exercise]):
+    conn.rollback()
     cursor.execute(
-        "DELETE FROM workout_exercise WHERE workout_id = %s", [str(id)])
+        "DELETE FROM workout_crud.workout_exercise WHERE workout_id = %s", [str(id)])
 
     for exercise in exercises:
-        cursor.execute("""INSERT INTO workout_exercise (workout_id, exercise, reps, sets) VALUES (%s, %s, %s, %s) """,
+        cursor.execute("""INSERT INTO workout_crud.workout_exercise (workout_id, exercise, reps, sets) VALUES (%s, %s, %s, %s) """,
                        (id, exercise.exercise, exercise.reps, exercise.sets))
         cursor.execute(
-            """ SELECT exercise FROM workout_exercise WHERE exercise = %s""", [exercise.exercise])
+            """ SELECT exercise FROM workout_crud.workout_exercise WHERE exercise = %s""", [exercise.exercise])
         workoutList = cursor.fetchall()
-        mydb.commit()
+        conn.commit()
         if not workoutList:
             return {'message': 'exercise not created'}
     return {'message': 'all worked out'}
